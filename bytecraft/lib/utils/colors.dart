981287832d9651b91bf741cc30b5e192c390c8e8
Piyush Patel <piyushpatel1746@gmail.com>
2023-06-30 01:38:25 +0530

    Added the Project from Local

---
+++ bytecraft/lib/utils/colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppColors {
+  static Color textColor = const Color(0xFFccc7c5);
+  static Color mainColor = const Color(0xFF89dad0);
+  static Color iconColor1 = const Color(0xFFffd28d);
+  static Color iconColor2 = const Color(0xFFfcab86);
+  static Color paraColor = const Color(0xFF8f8378);
+  static Color buttonBackgroundColor = const Color(0xFFf7f6f4);
+  static Color signColor = const Color(0xFFa9a29f);
+  static Color mainBlackColor = const Color(0xFF5c524f);
+  static Color yellowColor = const Color(0xFFffd379);
+}
---
+++ bytecraft/lib/widgets/big_text.dart
@@ -0,0 +1,30 @@
+import 'package:bytecraft/utils/dimensions.dart';
+import 'package:flutter/material.dart';
+
+class BigText extends StatelessWidget {
+  Color? color;
+  final String text;
+  double size = 0;
+  TextOverflow overflow;
+
+  BigText(
+      {super.key,
+      this.color = const Color(0xFF332d3b),
+      required this.text,
+      this.size = 20,
+      this.overflow = TextOverflow.ellipsis});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      maxLines: 1,
+      overflow: overflow,
+      style: TextStyle(
+          fontFamily: 'Roboto',
+          fontSize: size == 0 ? Dimensions.font20 : size,
+          color: color,
+          fontWeight: FontWeight.w400),
+    );
+  }
+}
